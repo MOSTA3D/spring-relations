0a992a30d806b2d8fc81c92d8c5bd887668420ec
something <some@thing.com>
2022-08-02 18:15:35 +0200

    student entity

--- src/main/java/com/cegebest/demo/ui/entities/Student.java
+++ src/main/java/com/cegebest/demo/ui/entities/Student.java
@@ -1,9 +1,12 @@
 package com.cegebest.demo.ui.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 public class Student {
@@ -23,41 +26,7 @@ public class Student {
 	@Column
 	private String name;
 	
-	@Column
-	private Long tid;
-	
-	@Column
-	private Long sid;
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Long getTid() {
-		return tid;
-	}
-
-	public void setTid(Long tid) {
-		this.tid = tid;
-	}
-
-	public Long getSid() {
-		return sid;
-	}
-
-	public void setSid(Long sid) {
-		this.sid = sid;
-	}
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="address_id", referencedColumnName = "id")
+	private Subject subject;
 }
